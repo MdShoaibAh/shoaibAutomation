8925a5a61c685aea46fa458214b74fb106c94d67
MdShoaibAh <91001774+MdShoaibAh@users.noreply.github.com>
2021-09-20 14:34:04 +0500

    Update test_case.py

--- components/adt/tests/test_case.py
+++ components/adt/tests/test_case.py
@@ -1,11 +1,11 @@
 def test_fun():
     print("test 1")
-    assert 10!=11
+    assert 10==11
 
 def test_fun1():
     print("test 2")
-    assert 10==10
+    assert 10!=10
 
 def test_fun2():
     print("test 3")
-    assert "10" == "10"
+    assert "10" == "hello"
